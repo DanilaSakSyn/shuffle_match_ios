b570e4193b1e4fd968a50a51c04138ea047ffa28
DanilaSakSyn <danilasak@syndicate-g.com>
2025-11-21 22:28:45 +0300

    rename notifications

--- fastlane/patch_embed_extension.rb
+++ fastlane/patch_embed_extension.rb
@@ -2,7 +2,7 @@ require 'xcodeproj'
 
 PROJECT_PATH      = ARGV[0] || 'build/iOS/iOS/Unity-iPhone.xcodeproj'
 MAIN_TARGET_NAME  = ARGV[1] || 'Unity-iPhone'
-EXT_TARGET_NAME   = ARGV[2] || 'notifications'
+EXT_TARGET_NAME   = ARGV[2] || 'notification-service'
 
 puts "Patch Xcodeproj: #{PROJECT_PATH}"
 
